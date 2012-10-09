a87ce44af41cc53a260c6fc62ff0aac106e6b432
craigp <craigp@baraka>
2012-10-09 23:50:56 +1300

    Fixed browser compatibility

--- wit/__init__.py
+++ wit/__init__.py
@@ -18,6 +18,7 @@ http://www.blogs.uni-osnabrueck.de/rotapken/2008/12/03/create-screenshots-of-a-w
 http://thechangelog.com/post/512693979/webkit2png-command-line-web-screenshots
 """
 import sys
+import signal
 import time
 
 try:
@@ -29,13 +30,28 @@ except ImportError:
   print("maybe try? 'sudo apt-get install python-qt4 libqt4-webkit'")
   sys.exit(1)
 
+class MobileBrowser(QWebPage):
+  def userAgentForUrl(self, url):
+    return "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+
+class DesktopBrowser(QWebPage):
+  def userAgentForUrl(self, url):
+    return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4";
+
+
 class Webshot(QObject):
-  def __init__(self, url=None, out=None, width=1024, height=None):
+  def __init__(self, url=None, out=None, width=1024, height=None, browser=None):
     QObject.__init__(self)
+
+    if browser is None:
+      browser = 'desktop'
+
+    self.browser = browser
 
     if not url:
       quit()
     self.url = url
+
     if not out:
       out = "web2png.%i.png"%int(time.time())
 
@@ -47,13 +63,25 @@ class Webshot(QObject):
 
   def shot(self):
     self.view = QWebView()
+
+    if self.browser is not None:
+      if self.browser == 'desktop':
+        self.view.setPage(DesktopBrowser())
+      else:
+        self.view.setPage(MobileBrowser())
+
     self.view.load(QUrl(self.url))
     self.view.show()
+
     self.page = self.view.page()
-    self.connect(self.page, SIGNAL("loadFinished(bool)"), self.render)
+    self.connect(self.view.page(), SIGNAL("loadFinished(bool)"), self.render)
 
   def render(self):
-    frame = self.page.currentFrame()
+    frame = self.view.page().currentFrame()
+
+    frame.setScrollBarPolicy(Qt.Horizontal, Qt.ScrollBarAlwaysOff)
+    frame.setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAlwaysOff)
+
     size = frame.contentsSize()
 
     if self.width is not None:
@@ -67,24 +95,26 @@ class Webshot(QObject):
       size.setHeight(size.height())
 
     self.view.resize(size)
-    self.page.setViewportSize(size)
+    self.view.page().setViewportSize(size)
 
     img = QImage(size, QImage.Format_ARGB32)
     paint = QPainter(img)
-
     frame.render(paint, QWebFrame.ContentsLayer)
     paint.end()
     img.save(self.out)
-    QApplication.quit()
+
+    sys.exit(0)
 
 
-def grab(src, dst, width=None, height=None):
+def grab(src, dst, width=None, height=None, browser='desktop'):
   """
   Wrapper around the class to easily GRAB an image
   when given a web page
   """
-  app = QApplication([])
-  t = Webshot(src, dst, width, height)
+  app = QApplication(sys.argv)
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+  t = Webshot(src, dst, width, height, browser)
   t.shot()
   return app.exec_()
 
